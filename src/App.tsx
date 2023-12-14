843cda294305658fe5ced22259821150f128cee2
Thasyo Peres <thasyotp2002@gmail.com>
2023-12-14 07:14:52 -0300

    creating addTask function

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+//react
+import { useState } from "react"
+
 //components
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -7,14 +10,20 @@ import TaskList from "./components/TaskList";
 // CSS
 import styles from "./App.module.css"
 
+// Interface
+import { ITask } from "./interfaces/Task"
+
 function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([])
+
   return (
     <div>
       <Header/>
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText="Criar Tarefa"/>
+          <TaskForm btnText="Criar Tarefa" taskList={taskList} setTaskList={setTaskList}/>
         </div>
         <div>
           <h2>Lista de Tarefas:</h2>
--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -8,16 +8,29 @@ import { ITask } from "../interfaces/Task"
 
 interface Props {
     btnText: string
+    taskList: ITask[]
+    setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>> //esta tipagem diz que estou recebendo o set de um useState.
 }
 
-const TaskForm = ({btnText}: Props) => {
+const TaskForm = ({btnText, taskList, setTaskList}: Props) => {
 
   const [id, setId] = useState<number>(0)
   const [title, setTitle] = useState<string>("")
   const [difficulty, setDifficulty] = useState<number>(0)
 
-  const addTaskHandler = () => {
+  const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
 
+    //add tasks
+    const id = Math.floor(Math.random() * 1000)
+    const newTask: ITask = {id, title, difficulty}
+    setTaskList!([...taskList, newTask])
+
+    //form values
+    setTitle("")
+    setDifficulty(0)
+
+    console.log(taskList)
   }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -27,19 +40,17 @@ const TaskForm = ({btnText}: Props) => {
     }else{
       setDifficulty(parseInt(e.target.value))
     }
-    console.log(title)
-    console.log(difficulty)
   }
 
   return (
     <form className={styles.form} onSubmit={addTaskHandler}>
         <div className={styles.input_container}>
             <label htmlFor='title'>Título:</label>
-            <input type="text" name='title' placeholder='Título da tarefa' onChange={handleChange}/>
+            <input type="text" name='title' placeholder='Título da tarefa' value={title} onChange={handleChange}/>
         </div>
         <div className={styles.input_container}>
             <label htmlFor='difficulty'>Dificuldade:</label>
-            <input type="text" name='difficulty' placeholder='Dificuldade da tarefa' onChange={handleChange}/>
+            <input type="text" name='difficulty' placeholder='Dificuldade da tarefa' value={difficulty} onChange={handleChange}/>
         </  div>
         <input type="submit" value={btnText}/>
     </form>
