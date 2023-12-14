3ae25ad36ade8d0c4c03d5c795fbaefdfaa90f4d
Thasyo Peres <thasyotp2002@gmail.com>
2023-12-14 07:26:58 -0300

    Displaying data on the screen

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
         </div>
         <div>
           <h2>Lista de Tarefas:</h2>
-          <TaskList/>
+          <TaskList taskList={taskList}/>
         </div>
       </main>
       <Footer/>
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 
-type Props = {}
+// Interface
+import {ITask} from "../interfaces/Task"
 
-const TaskList = (props: Props) => {
+// CSS
+import styles from "./TaskList.module.css"
+
+interface Props {
+  taskList: ITask[]
+}
+
+const TaskList = ({taskList}: Props) => {
   return (
-    <div>TaskList</div>
+    <>
+    
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id}>
+            <p>{task.title}</p>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas!</p>
+      )}
+
+    </>
   )
 }
 
